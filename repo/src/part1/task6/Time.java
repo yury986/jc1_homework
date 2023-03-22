ce497be95909b72af06c5e62dea7ca8343ee2500
Yury Tomko <yury_986@mail.ru>
2023-03-22 19:45:20 +0300

    time

--- repo/src/part1/task6/Time.java
+++ repo/src/part1/task6/Time.java
@@ -7,7 +7,7 @@ public class Time {
         int m, min;
         int h, hour;
         int d, day;
-        int w, week;
+        int w;
         sec = s % 60;
         m = (s - sec) / 60;
         min = m % 60;
@@ -16,8 +16,7 @@ public class Time {
         d = (h - hour) / 24;
         day = d % 7;
         w = (d - day) /7;
-        week = w % 7;
-        System.out.println(week + "недели" + " " + day  + "суток" + " " + hour + "часов" + " " + min + "минут" + " " + sec + "секунд");
+        System.out.println ( w + "недели" + " " + day  + "суток" + " " + hour + "часов" + " " + min + "минут" + " " + sec + "секунд");
 
 
     }
