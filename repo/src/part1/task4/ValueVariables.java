19dcb8808bb27ef854406001bff1ed1efd3a306e
Yury Tomko <yury_986@mail.ru>
2023-03-22 18:14:02 +0300

    variables

---
+++ repo/src/part1/task4/ValueVariables.java
@@ -0,0 +1,22 @@
+package part1.task4;
+
+public class ValueVariables {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        short s =0x55ff;
+        int i =1000000;
+        long I = 0xffffffffL;
+        char c = 'a';
+        float f = .25f;
+        double d = .00001234;
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+    }
+}
