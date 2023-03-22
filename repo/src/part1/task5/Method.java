f395b12ee99d4ed9328353b762ae0e0b31ddff89
Yury Tomko <yury_986@mail.ru>
2023-03-22 18:59:19 +0300

    calc

---
+++ repo/src/part1/task5/Method.java
@@ -0,0 +1,13 @@
+package part1.task5;
+
+public class Method {
+    public static void main(String[] args) {
+        int x = calc (2,3);
+
+        System.out.println(x);
+    }
+        static int calc (int a, int b ){
+            return (a + b) + a * b;
+        }
+    }
+
