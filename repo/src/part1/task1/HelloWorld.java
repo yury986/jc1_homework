590e49704c8895a9d1805ea945c98974d401ba38
Yury Tomko <yury_986@mail.ru>
2023-03-22 17:23:56 +0300

    new

--- repo/src/part1/task1/HelloWorld.java
+++ repo/src/part1/task1/HelloWorld.java
@@ -2,6 +2,6 @@ package part1.task1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Wor");
+        System.out.println("Hello World!");
     }
 }
--- repo/src/part1/task2/HelloWorld.java
+++ repo/src/part1/task2/HelloWorld.java
@@ -1,4 +1,7 @@
 package part1.task2;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus!");
+    }
 }
