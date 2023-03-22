a2ddfce6c218ca132d9af4ea8d3a76bbfcb11e09
Yury Tomko <yury_986@mail.ru>
2023-03-22 17:42:32 +0300

    I like Java

--- repo/src/part1/task3/I_like_Java.java
+++ repo/src/part1/task3/I_like_Java.java
@@ -3,6 +3,6 @@ package part1.task3;
 public class I_like_Java {
     public static void main(String[] args) {
         System.out.println("Hello Belarus!");
-        System.out.println("I like Java!");
+        System.out.println("I like  Java!");
     }
 }
