7146e594c5de7e6f4bcc7c220f1f915d25c2a462
Yury Tomko <yury_986@mail.ru>
2023-03-28 17:57:34 +0300

    first commit

--- repo/src/part1/task11/Houses.java
+++ repo/src/part1/task11/Houses.java
@@ -2,16 +2,17 @@ package part1.task11;
 
 public class Houses {
     public static void main(String[] args) {
-        int a = 6;   // Длина
-        int b = 4;   // Ширина
-        int c = 7;   // Длина
-        int d = 5;   // Ширина
-        int e = 7;   // Длина
-        int f = 9;   // Ширина
+        int a = 10;
+        int b = 3;
+        int c = 10;
+        int d = 3;
+        int e = 6;
+        int f = 10;
             // Дома распологаются параллельно        Дома распологаются в длину
             // друг другу и ширины складываются      длины складываются а из ширин
             // а из длин находится максимальная.     находится максимальная.
-        if ((f >= b + d && e >= a && e >= c)||(e >= a + c && f >= b && f >= d)) {
+        if ((f >= b + d && e >= a && e >= c)||(e >= a + c && f >= b && f >= d)||
+                (e >= b+d && f >= a && f >=c)||(f >= a+c && e >= b && f >= d)){
 
             System.out.println("Дома помещаются.");
         }else{
