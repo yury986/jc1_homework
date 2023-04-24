34309234391b175b655cd39cd9fddd578b137069
Yury Tomko <yury_986@mail.ru>
2023-04-24 20:38:40 +0300

    first commit

--- repo/src/part1/task9/Money.java
+++ repo/src/part1/task9/Money.java
@@ -2,8 +2,10 @@ package part1.task9;
 
 public class Money {
     public static void main(String[] args) {
-        int cash = 205;
-        if (cash % 10 == 1) {
+        int cash = 120;
+        if (cash % 100 >= 11 && cash % 100 <= 14) {
+            System.out.println(cash + " " + "рублей.");
+        }else if (cash % 10 == 1) {
             System.out.println(cash + " " + "рубль.");
         }else if (cash % 10 >= 2 && cash % 10 <= 4){
             System.out.println(cash + " " + "рубля.");
@@ -12,6 +14,5 @@ public class Money {
         }else if (cash<0){
             System.out.println("Ошибка");
         }
-
     }
 }
