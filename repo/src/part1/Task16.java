c49ad6c27d312c51849e1805e8ad40834c77f566
Yury Tomko <yury_986@mail.ru>
2023-04-26 20:48:41 +0300

    first commit

--- repo/src/part1/Task16.java
+++ repo/src/part1/Task16.java
@@ -2,15 +2,24 @@ package part1;
 
 public class Task16 {
     public static void main(String[] args) {
-        int c = 0;
-        for ( int i = 50; i<=70; i++){
-            if (!(i%2==0 || i%3==0)){
-                c=c+1;
-                if (c==2){
+        int sum =0;
+        boolean b = true;
+        for (int P = 50; P <= 70; P++) {
+            for (int i = 2; i < P; i++) {
+                if (P % i == 0) {
+                    b = false;
                     break;
                 }
-                System.out.println(i);
             }
+            if (b) {
+                sum = sum+1;
+            }
+            if(sum == 2){
+                System.out.println(P);
+                System.exit(0);
+            }else b = true;
+
         }
     }
 }
+
