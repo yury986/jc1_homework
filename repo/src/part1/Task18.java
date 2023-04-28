c96b01f729bcdcabce46a5abec1a86d46436bfba
Yury Tomko <yury_986@mail.ru>
2023-04-28 18:48:14 +0300

    second commit

--- repo/src/part1/Task18.java
+++ repo/src/part1/Task18.java
@@ -2,27 +2,19 @@ package part1;
 
 public class Task18 {
     public static void main(String[] args) {
-        int a = 20023143;
-        int f = a;
-        int c = 1;
-        int b;
-        int res = 0;
-        while (a!=0){
-           ++ res;
-           a/=1000;
-        }
-
-        for (int j = 0; j< res; j++){
-            c*=1000;
-        }
-
-        for (int i = 0; i<res; i++) {
-            b = (f % c) / (c / 1000);
-            c = c / 1000;
-
-            System.out.print(b + " ");
-        }
-
+            int num = 20023143;
+            String str = "";
+            int i = 0;
+            while (num > 0) {
+                int j = num % 10;
+                str = j + str;
+                num = num / 10;
+                i++;
+                if (i % 3 == 0 && num != 0) {
+                    str = " " + str;
+                }
+            }
+            System.out.println(str);
         }
     }
 
